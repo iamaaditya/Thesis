a0394b15b51a83eb992be1c1f093c81703a2119c
aaditya prakash <aaadityaprakash@gmail.com>
2019-02-25 01:09:40 -0500

    upate

--- backup_thesis_command.sh
+++ backup_thesis_command.sh
@@ -1,1 +1,1 @@
-cp ../../Documents/thesis/chapters/*.tex chapters/ ; cp ../../Documents/thesis/*.tex . ; git add *; git add chapters; git commit -am "upate"; git push origin master
+cp ../../Documents/thesis/chapters/*.tex chapters/ ; cp ../../Documents/thesis/*.tex . ; git add *; git add chapters; git commit -am "upate"; git push origin master
