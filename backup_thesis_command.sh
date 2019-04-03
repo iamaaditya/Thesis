882b594b0a50ed6d0a437c2042058276dd300292
aaditya prakash <aaadityaprakash@gmail.com>
2019-04-03 03:12:15 -0400

    update

--- backup_thesis_command.sh
+++ backup_thesis_command.sh
@@ -1,1 +1,1 @@
-git add *; git add chapters; git commit -am "update"; git push origin master
+git add *; git add chapters; git add figures/*; git add figures/intro/*; git commit -am "update"; git push origin master
